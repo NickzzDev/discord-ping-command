942c69d83e2717fe00bcf834c9c2e66e6c4d02cb
Nickzz <85542797+NickzzDev@users.noreply.github.com>
2021-09-26 18:24:11 +1000

    Create v12.js

---
+++ v12.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+
+exports.run = async (client, msg, args) => {
+    const ping = new Discord.MessageEmbed()
+    .setDescription(`🏓 Pong! 🏓\n
+    The Bot\'s Ping is \`${client.ws.ping}\` ping \n\nCreated by [Nickzz](https://github.com/NickzzDev)`)
+    msg.reply(ping)
+}
