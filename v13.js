e6a4327202d41d57438ffb5f6312cf19a03a3eb6
Nickzz <85542797+NickzzDev@users.noreply.github.com>
2021-09-26 18:24:24 +1000

    Update v13.js

--- v13.js
+++ v13.js
@@ -3,6 +3,6 @@ const Discord = require('discord.js');
 exports.run = async (client, msg, args) => {
     const ping = new Discord.MessageEmbed()
     .setDescription(`🏓 Pong! 🏓\n
-    The Bot\'s Ping is \`${client.ws.ping}\` ping`)
+    The Bot\'s Ping is \`${client.ws.ping}\` ping \n\nCreated by [Nickzz](https://github.com/NickzzDev)`)
     msg.reply({ embeds: [ping] })
 }
